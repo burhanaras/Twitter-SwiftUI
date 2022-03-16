49066e94d1a8b2fb1e39d9a140052616e8d29801
Burhan Aras <burhanaras@live.com>
2022-03-16 23:18:04 +0300

    ViewModel functions are implemented.

--- Twitter-SwiftUI/Twitter-SwiftUI/UI/Feed/FeedViewModel.swift
+++ Twitter-SwiftUI/Twitter-SwiftUI/UI/Feed/FeedViewModel.swift
@@ -12,8 +12,9 @@ class FeedViewModel: ObservableObject {
     
     @Published private(set) var users = [User]()
     @Published private(set) var posts = [Post]()
+    @Published private(set) var currentUser = User.fromDTO(dto: DummyData.dummyUsers()[0])
     private var allPosts = [Post]()
-
+    
     private var cancellables: Set<AnyCancellable> = []
     private var networkLayer: INetworkLayer = InMemoryNetworkLayer()
     
@@ -22,10 +23,53 @@ class FeedViewModel: ObservableObject {
     }
     
     func loadData() {
-        
+        var _: AnyCancellable? = Publishers.Zip(
+            networkLayer.getUsers(),
+            networkLayer.getAllPosts()
+        )
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                        
+                    case .finished:
+                        break
+                    case let .failure(error):
+                        print("ERROR Here: \(error.localizedDescription)")
+                    }
+                },
+                receiveValue: { [unowned self] usersResponse, postsResponse in
+                    print("\(usersResponse.count) users downloaded.")
+                    self.users = usersResponse.map { User.fromDTO(dto: $0) }
+                    self.currentUser = users[0]
+                    
+                    print("\(postsResponse.count) posts downloaded")
+                    self.allPosts = postsResponse.map { Post.fromDTO(dto: $0)}
+                    filter(userName: users[0].username)
+                }
+            )
     }
     
-    func post(text: String) { }
+    func post(text: String) {
+        let post = Post(id: UUID().uuidString, username: currentUser.username, name: currentUser.name, image: currentUser.image, text: text)
+        networkLayer.post(user: self.currentUser, post: post)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case let .failure(error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { postResponse in
+                print(postResponse)
+            }
+            .store(in: &cancellables)
+    }
     
-    func filter(userName: String) { }
+    func filter(userName: String) {
+        if let matchingUser = users.filter { $0.username == userName}.first {
+            self.currentUser = matchingUser
+            self.posts = allPosts.filter { $0.username == currentUser.username}
+        }
+    }
+    
 }
