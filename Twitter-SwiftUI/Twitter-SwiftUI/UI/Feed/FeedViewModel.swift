312588e38caf04226da9cef6aaf9b131d6a6165e
Burhan Aras <burhanaras@live.com>
2022-03-16 23:09:10 +0300

    ViewModel api is ready.

--- Twitter-SwiftUI/Twitter-SwiftUI/UI/Feed/FeedViewModel.swift
+++ Twitter-SwiftUI/Twitter-SwiftUI/UI/Feed/FeedViewModel.swift
@@ -6,7 +6,26 @@
 //
 
 import Foundation
+import Combine
 
 class FeedViewModel: ObservableObject {
     
+    @Published private(set) var users = [User]()
+    @Published private(set) var posts = [Post]()
+    private var allPosts = [Post]()
+
+    private var cancellables: Set<AnyCancellable> = []
+    private var networkLayer: INetworkLayer = InMemoryNetworkLayer()
+    
+    init(networkLayer: INetworkLayer){
+        self.networkLayer = networkLayer
+    }
+    
+    func loadData() {
+        
+    }
+    
+    func post(text: String) { }
+    
+    func filter(userName: String) { }
 }
