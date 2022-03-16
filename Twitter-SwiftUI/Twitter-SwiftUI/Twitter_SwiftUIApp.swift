a77bd09a41cdb8df587545a7e6c1c17b14f14689
Burhan Aras <burhanaras@live.com>
2022-03-16 22:49:15 +0300

    Bismillah. Initial commit.

---
+++ Twitter-SwiftUI/Twitter-SwiftUI/Twitter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUIApp.swift
+//  Twitter-SwiftUI
+//
+//  Created by Burhan Aras on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
